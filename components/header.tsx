ce9a81379144bd120ab1c4529ebda5ee9f5d4d84
Qerkoshi <qerkoshi@proton.me>
2026-01-02 14:32:02 +0100

    feat: Header component should use ellipsis for long content

--- components/header.tsx
+++ components/header.tsx
@@ -12,25 +12,27 @@ export const Header: FC<{
 
 	return (
 		<div
-			className="text-center fixed left-0 top-0 right-0 bg-background shadow-xs"
+			className="fixed left-0 top-0 right-0 bg-background shadow-xs"
 			style={{
 				paddingTop: "env(safe-area-inset-top)",
 			}}
 		>
 			<div className="relative flex flex-row w-full justify-center">
-				<div className="max-w-xl px-4 py-3 flex flex-1 flex-row justify-start items-center gap-6">
+				<div className="max-w-xl px-4 py-3 flex flex-1 flex-row justify-start items-center gap-6 w-full">
 					<Button
 						variant={"secondary"}
 						onClick={props.onBackClick ?? (() => router.back())}
 					>
 						<ArrowLeftIcon />
 					</Button>
-					<div className={"flex flex-1 justify-between items-center"}>
-						<h2 className="text-2xl font-bold text-foreground">
-							{props.title}
-						</h2>
-						{props.endAddon}
-					</div>
+					<h2
+						className={
+							"flex-1 justify-between items-center overflow-hidden text-xl font-bold text-foreground text-nowrap text-ellipsis inline-block"
+						}
+					>
+						{props.title}
+					</h2>
+					{props.endAddon}
 				</div>
 			</div>
 		</div>
